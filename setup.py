74aa41c031b7fb7b035ef1c0e94a1698f3c1064b
Matt Jenior <mljenior@umich.edu>
2018-11-04 14:52:51 -0500

    getting pip install established

--- setup.py
+++ setup.py
@@ -2,4 +2,10 @@
 
 from setuptools import setup
 
-setup(name='riptide', version='0.1', scripts=['riptide'])
+setup(name='riptide',
+	version='0.1', 
+	description='Reaction Inclusion by Parsimonious usage, Transcript Distribution, and Exploration of topology',
+	author='Matt Jenior',
+	author_email='mattjenior@gmail.com',
+	url='https://github.com/mjenior/riptide',
+	scripts=['riptide'])
