72b87e48ab67b08e5956e2b443d96469aef5cfeb
Matt Jenior <mattjenior@gmail.com>
2019-01-16 15:25:59 -0500

    updated documentation

--- riptide.py
+++ riptide.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python
 
-# Please cite when using:
-# Jenior ML and Papin JA. (2018) RIPTiDe: Metabolic model contextualization through exploration 
-# of mutliple transcription and flux minimization levels. BioRxiv. DOI
-
-# Example usage:
-# my_model = cobra.io.read_sbml_model('my_model.sbml')
-# transcript_abundances = read_transcription_file('reads_to_genes.tsv', replicates=True)
-# contextualized_model = riptide(my_model, transcript_abundances, ...)
-
 # Dependencies
 import copy
 import time
