af10f13471759adcc2d01501c02b01a9136b6ed7
Matt Jenior <mattjenior@gmail.com>
2019-04-18 14:59:05 -0400

    tweaking ReadMe

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='riptide',
-	version='0.8', 
+	version='0.9', 
 	description='Reaction Inclusion by Parsimony and Transcript Distribution (RIPTiDe)',
 	author='Matt Jenior',
 	author_email='mattjenior@gmail.com',
 	url='https://github.com/mjenior/riptide',
 	packages=['riptide'],
     license='MIT',
-    long_description=open('README.txt').read()
+    long_description=open('README.md').read()
 	)
