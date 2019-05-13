a601c6078ac47fba7d3ae19ff2687754b9ee7827
Matt Jenior <mattjenior@gmail.com>
2019-05-13 12:39:15 -0400

    fixed readme packaging and rendering

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+import setuptools
 
-setup(name='riptide',
-	version='1.7.3', 
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+
+setuptools.setup(
+	name='riptide',
+	version='1.7.5', 
 	description='Reaction Inclusion by Parsimony and Transcript Distribution (RIPTiDe)',
 	author='Matthew Jenior',
 	author_email='mattjenior@gmail.com',
 	url='https://github.com/mjenior/riptide',
-	packages=['riptide'],
+	packages=setuptools.find_packages(),
     install_requires=[
           'cobra',
           'symengine',
     ],
     license='MIT',
-    long_description=open('README.rst').read()
+    long_description_content_type='text/markdown',
+    long_description=long_description,
 	)
