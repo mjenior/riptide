476af74894e6e2133058512da8771cf1b4d7ef1e
Matt Jenior <mattjenior@gmail.com>
2019-07-25 14:47:31 -0400

    added ensemble inactivity detection, repackaged

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme:
     long_description = readme.read()
 
 setup(name='riptide',
-	version='2.0.6', 
+	version='2.1.0', 
 	description='Reaction Inclusion by Parsimony and Transcript Distribution (RIPTiDe)',
 	author='Matthew Jenior',
 	author_email='mattjenior@gmail.com',
