999c74cdb3bf2c01c411686634cc4945afe23605
mjenior-zymergen <107438341+mjenior-zymergen@users.noreply.github.com>
2023-01-24 13:36:22 -0500

    flux sampling check fix

--- src/riptide.py
+++ src/riptide.py
@@ -695,7 +695,7 @@ def contextualize(model, transcriptome = 'none', samples = 1000, silent = False,
     riptide_object.concordance = concordance
 
     # Assign new reaction bounds
-    if set_bounds == True and flux_samples != 'Not performed':
+    if set_bounds == True and isinstance(flux_samples, str) == False:
         for rxn in riptide_model.reactions:
             current_dist = list(flux_samples[rxn.id])
             
